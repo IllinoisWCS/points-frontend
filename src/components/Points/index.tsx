a7974610ae3c2397ad38e0954c11b2192df2602c
Akash Sannasi <sannasi2@illinois.edu>
2023-03-21 17:51:50 -0500

    Home page shows points

--- src/components/Points/index.tsx
+++ src/components/Points/index.tsx
@@ -18,16 +18,6 @@ import { PointsProps } from './types';
 const Points = (props: PointsProps): React.ReactElement => {
   const { isLoading, isError, error, data } = props;
 
-  console.log(isLoading, data);
-
-  // const { isLoading, isError, error, data} = useQuery<Profile, Error>(
-  //   ['get-profile'],
-  //   async () => {
-  //     const res = await axiosInstance.get('/profile');
-  //     return res.data;
-  //   }
-  // );
-
   if (isError) {
     console.log(error);
     return (
@@ -38,42 +28,48 @@ const Points = (props: PointsProps): React.ReactElement => {
   }
 
   return (
-    <Box maxHeight="400px" overflowY="scroll" paddingTop="5px">
+    <Box>
       <Heading size="lg">Points</Heading>
-      <Center mb="5">
-        <Text fontSize="xl">
-          {`You have ${data?.points ?? 0} ${
-            data?.points === 1 ? 'point' : 'points'
-          }.`}
-        </Text>
-      </Center>
-      <Skeleton startColor="gray.100" endColor="gray.200" isLoaded={!isLoading}>
-        <Stack
-          divider={<StackDivider />}
-          borderRadius="10"
-          border="1px"
-          borderColor="gray.100"
+      <Box maxHeight="380px" overflowY="scroll" paddingTop="5px">
+        <Center mb="5">
+          <Text fontSize="xl">
+            {`You have ${data?.points ?? 0} ${
+              data?.points === 1 ? 'point' : 'points'
+            }.`}
+          </Text>
+        </Center>
+        <Skeleton
+          startColor="gray.100"
+          endColor="gray.200"
+          isLoaded={!isLoading}
         >
-          {data?.events?.map((event, id) => (
-            <HStack justify="space-between" key={id} p="5">
-              <Stack>
-                <Text fontSize="lg" fontWeight="medium">
-                  {event.name}
-                </Text>
-                <Text className="muted">{getEventDate(event)}</Text>
-              </Stack>
-              <VStack>
-                <Text fontSize="l" fontWeight="medium">
-                  {event.points}
-                </Text>
-                <Text className="muted">
-                  {event.points === 1 ? 'point' : 'points'}
-                </Text>
-              </VStack>
-            </HStack>
-          ))}
-        </Stack>
-      </Skeleton>
+          <Stack
+            divider={<StackDivider />}
+            borderRadius="10"
+            border="1px"
+            borderColor="gray.100"
+          >
+            {data?.events?.map((event, id) => (
+              <HStack justify="space-between" key={id} p="5">
+                <Stack>
+                  <Text fontSize="lg" fontWeight="medium">
+                    {event.name}
+                  </Text>
+                  <Text className="muted">{getEventDate(event)}</Text>
+                </Stack>
+                <VStack>
+                  <Text fontSize="l" fontWeight="medium">
+                    {event.points}
+                  </Text>
+                  <Text className="muted">
+                    {event.points === 1 ? 'point' : 'points'}
+                  </Text>
+                </VStack>
+              </HStack>
+            ))}
+          </Stack>
+        </Skeleton>
+      </Box>
     </Box>
   );
 };
