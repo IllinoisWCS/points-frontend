689aae64fce7d6d44da92ab9847c0eeaba446dbb
Camryn <camryyn.lee@gmail.com>
2024-12-08 13:39:20 -0600

    Cl/edit icon (#34)
    * adding explicit logout login button
    
    * removing automatic redirect
    
    * Add edit icon to events only visible to officers
    
    * Remove extraneous commits
    
    * Make it conditional to officers

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,10 +13,9 @@ import {
 import { FiHome, FiTrendingUp, FiCalendar } from 'react-icons/fi';
 import { DarkModeToggle } from 'react-dark-mode-toggle-2';
 import { useQuery } from 'react-query';
-
 import axiosInstance from '../../api';
-import { LinkItemProps, NavbarProps, NavItemProps } from './types';
 import { Profile } from '../../types/profile';
+import { LinkItemProps, NavbarProps, NavItemProps } from './types';
 import Logo from '../Logo';
 
 const LinkItems: LinkItemProps[] = [
