57ea1e865c471be2c98fdef052664f05217d5de7
helenailic <helenailic278@gmail.com>
2025-03-02 10:38:51 -0600

    fix conflict?

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -58,7 +58,7 @@ const Navbar = ({ onClose, ...rest }: NavbarProps): React.ReactElement => {
     }
   );
 
-  const Greeting = (): React.ReactElement | null => {
+  const Greeting = (): React.ReactElement => {
     if (!data) return null;
 
     const names = data?.name.split(' ');
