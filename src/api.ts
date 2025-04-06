5213638d2f714594edc90a9b472c415bed90e34b
Helena Ilic <70663784+helenailic@users.noreply.github.com>
2025-04-06 11:07:25 -0500

    Hi/logging fixes (#59)
    * fixing non logging issue when on same page!!
    
    * api

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL
     ? import.meta.env.VITE_BASE_URL
-    : // : 'http://127.0.0.1:3000',
+    : // 'http://127.0.0.1:3000',
       'https://points-api.illinoiswcs.org',
   withCredentials: true
 });
