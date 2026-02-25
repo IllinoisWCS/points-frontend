37ad8d45388d806107085f6d6b8483eb6e5b5741
aliyaa23 <71795590+aliyaa23@users.noreply.github.com>
2026-02-25 17:04:52 -0600

    added not-authorized page (#106)
    * added not-authorized page
    
    * updated not authorized toat popup
    
    * Fix toastError message formatting
    
    ---------
    
    Co-authored-by: Helena Ilic <70663784+helenailic@users.noreply.github.com>

---
+++ src/components/PointTracker/NotAuthorized.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react';
+import { Box, Heading, Text, Button, VStack } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { toastError } from '../../utils/toast';
+
+const NotAuthorizedPage: React.FC = (): React.ReactElement => {
+  const navigate = useNavigate();
+  const hasToasted = useRef(false);
+
+  useEffect(() => {
+    if (!hasToasted.current) {
+      toastError('Please find an officer to scan your redemption QR code');
+      hasToasted.current = true;
+    }
+  }, []);
+
+  return (
+    <Box
+      p={5}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      h="100vh"
+    >
+      <VStack spacing={5} textAlign="center">
+        <Heading size="2xl" color="pink.500">
+          Not Authorized
+        </Heading>
+        <Text fontSize="lg" color="gray.600">
+          Only officers can verify and redeem vintage merchandise.
+        </Text>
+        <Button
+          colorScheme="pink"
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          Go to Home
+        </Button>
+      </VStack>
+    </Box>
+  );
+};
+
+export default NotAuthorizedPage;
