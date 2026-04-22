a9c2819861dd9f331663e16c32f885dc3d9d17d3
helenailic <helenailic278@gmail.com>
2026-04-22 13:25:59 -0500

    testing out badge modal - nice!

--- src/pages/Badges/index.tsx
+++ src/pages/Badges/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import BadgeContainer from '../../components/Badges/BadgeContainer';
+import BadgeModal from '../../components/Badges/BadgeModal';
+import allrounder from '../../assets/badges/all_rounder.png';
 
 const Badges = (): React.ReactElement => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+
   return (
     <>
       <BadgeContainer
         badgeId={''}
         image={''}
         isEarned={false}
-        onClick={function (): void {
-          throw new Error('Function not implemented.');
+        onClick={() => {
+          setModalOpen(true);
         }}
+      />
+      <BadgeModal
+        isOpen={modalOpen}
+        onClose={() => {
+          setModalOpen(false);
+        }}
+        titleText={'Template Badge Title'}
+        image={allrounder}
+        descriptionText={'This is a template badge description.'}
       />
     </>
   );
