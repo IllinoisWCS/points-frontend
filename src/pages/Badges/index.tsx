42c93c38e353479e5aab62e6db9dece3efff5cce
helenailic <helenailic278@gmail.com>
2026-05-30 20:40:03 -0700

    fixing imports

--- src/pages/Badges/index.tsx
+++ src/pages/Badges/index.tsx
@@ -1,50 +1,11 @@
 import React from 'react';
-
 import BadgeContainer from '../../components/Badges/BadgeContainer';
 import BadgeModal from '../../components/Badges/BadgeModal';
-import allrounder from '../../assets/badges/all_rounder.png';
-import corporate_consultant from '../../assets/badges/corporate_consultant.png';
-import corporate_insider from '../../assets/badges/corporate_insider.png';
-import corporate_intern from '../../assets/badges/corporate_intern.png';
-import explorations_navigator from '@assets/badges/explorations_navigator.png';
-import explorations_pioneer from '../../assets/badges/explorations_pioneer.png';
-import explorations_rookie from '../../assets/badges/explorations_rookie.png';
-import hello_world from '../../assets/badges/hello_world.png';
-import mentoring_beacon from '../../assets/badges/mentoring_beacon.png';
-import mentoring_guide from '../../assets/badges/mentoring_guide.png';
-import mentoring_sprout from '../../assets/badges/mentoring_sprout.png';
-import not_tiered from '../../assets/badges/not_tiered.png';
-import social_butterfly from '../../assets/badges/social_butterfly.png';
-import social_caterpillar from '../../assets/badges/social_caterpillar.png';
-import social_chrysalis from '../../assets/badges/social_chrysalis.png';
-import tier1 from '../../assets/badges/tier1.png';
-import tier2 from '../../assets/badges/tier2.png';
-import tier3 from '../../assets/badges/tier3.png';
-import { Heading, Box, Center, Text } from '@chakra-ui/react';
-import { Profile } from '../../types/profile';
+import { badgeImageMap } from '../../utils/badgeImageMap';
 import axiosInstance from '../../api';
+import { Box, Heading, Center, Text } from '@chakra-ui/react';
+import { Profile } from '../../types/profile';
 import { useQuery } from 'react-query';
-
-const badgeImageMap: Record<string, string> = {
-  'all_rounder.png': allrounder,
-  'corporate_consultant.png': corporate_consultant,
-  'corporate_insider.png': corporate_insider,
-  'corporate_intern.png': corporate_intern,
-  'explorations_navigator.png': explorations_navigator,
-  'explorations_pioneer.png': explorations_pioneer,
-  'explorations_rookie.png': explorations_rookie,
-  'hello_world.png': hello_world,
-  'mentoring_beacon.png': mentoring_beacon,
-  'mentoring_guide.png': mentoring_guide,
-  'mentoring_sprout.png': mentoring_sprout,
-  'not_tiered.png': not_tiered,
-  'social_butterfly.png': social_butterfly,
-  'social_caterpillar.png': social_caterpillar,
-  'social_chrysalis.png': social_chrysalis,
-  'tier1.png': tier1,
-  'tier2.png': tier2,
-  'tier3.png': tier3
-};
 
 const Badges = (): React.ReactElement => {
   const [badgeCatalog, setBadgeCatalog] = React.useState<Badge[]>([]);
@@ -110,7 +71,6 @@ const Badges = (): React.ReactElement => {
   const name = String(names?.[0] ?? '');
 
   const handleClick = async (): Promise<void> => {
-    // user clicked login
     window.location.href = `${String(
       axiosInstance.defaults.baseURL
     )}/auth/login`;
@@ -123,7 +83,7 @@ const Badges = (): React.ReactElement => {
         <Center mb="5">
           <Text fontSize="xl">
             {data ? (
-              'Attend WCS Events to Collect ‘Em All!'
+              'Attend WCS Events to Collect \u2019Em All!'
             ) : (
               <>
                 <a
@@ -132,14 +92,11 @@ const Badges = (): React.ReactElement => {
                     e.preventDefault();
                     void handleClick();
                   }}
-                  style={{
-                    color: '#E46167',
-                    textDecoration: 'underline'
-                  }}
+                  style={{ color: '#E46167', textDecoration: 'underline' }}
                 >
                   Login
                 </a>
-                {' to Collect ‘Em All!'}
+                {' to Collect \u2019Em All!'}
               </>
             )}
           </Text>
@@ -156,11 +113,11 @@ const Badges = (): React.ReactElement => {
                   ? badgeImageMap[badge.image] ?? ''
                   : badge.isTiered
                   ? badge.tier === 1
-                    ? tier1
+                    ? badgeImageMap['tier1.png']
                     : badge.tier === 2
-                    ? tier2
-                    : tier3
-                  : not_tiered
+                    ? badgeImageMap['tier2.png']
+                    : badgeImageMap['tier3.png']
+                  : badgeImageMap['not_tiered.png']
               }
               isEarned={earnedBadgeNames.includes(badge.name)}
               onClick={() => {
@@ -187,11 +144,11 @@ const Badges = (): React.ReactElement => {
               ? badgeImageMap[selectedBadge.image] ?? ''
               : selectedBadge.isTiered
               ? selectedBadge.tier === 1
-                ? tier1
+                ? badgeImageMap['tier1.png']
                 : selectedBadge.tier === 2
-                ? tier2
-                : tier3
-              : not_tiered
+                ? badgeImageMap['tier2.png']
+                : badgeImageMap['tier3.png']
+              : badgeImageMap['not_tiered.png']
           }
           descriptionText={
             earnedBadgeNames.includes(selectedBadge.name)
